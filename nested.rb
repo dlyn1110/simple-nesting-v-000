0f32f63bdc539189beeec283076f53d168769808
Dani Lyn <dlyn1110@gmail.com>
2018-04-24 02:34:55 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,15 +15,14 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+        programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 
 	programmer_hash =
- 		{
+ 		    {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -37,11 +36,12 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+		   programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
 	programmer_hash =
- 		{
+ 		   {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -55,15 +55,11 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		   programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-{
-programmer_hash = {
- 		:yukihiro_matsumoto => {
-    :known_for => "Ruby",
-    :languages => ["LISP", "C"]
- },
+    {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -77,7 +73,9 @@ programmer_hash = {
           :languages => ["C"]
         }
      }
+	 programmer_hash = [:yukihiro_matsumoto]= {:known_for => "Ruby", :languages => ["LISP", "C"]}
 
+				 :languages => ["LISP", "C"]
 
 end
 
