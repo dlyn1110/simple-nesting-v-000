26135631495b62766b778da54335493cfe43c132
Dani Lyn <dlyn1110@gmail.com>
2018-04-24 02:37:17 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -73,15 +73,14 @@ def adding_matz
           :languages => ["C"]
         }
      }
-	 programmer_hash = [:yukihiro_matsumoto]= {:known_for => "Ruby", :languages => ["LISP", "C"]}
-
-				 :languages => ["LISP", "C"]
+	   programmer_hash = [:yukihiro_matsumoto]= {:known_for => "Ruby", :languages => ["LISP", "C"]}
+     programmer_hash
 
 end
 
 def changing_alan
 	programmer_hash =
- 		{
+ 		   {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -96,7 +95,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
+      alans_new_info = "GUI"
+		  programmer_hash[:alan_kay][:known_for] = "GUI"
+			programmer_hash
 
 
 end
